2fea41ed7ab4e3650cf46302cfafe46f10f225b3
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2020-04-04 23:27:34 +0900

    新EdgeをEdgeChromiumImporterFactoryとして登録、旧Edgeを表示から削除

---
+++ SnkLib.App.CookieGetter/Net4.5/SnkLib.App.CookieGetter/Factories/EdgeChromiumImporterFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunokoLibrary.Application.Browsers
+{
+    /// <summary>
+    /// MicrosoftEdge(Chromium)からICookieImporterを取得します。
+    /// </summary>
+    public class EdgeChromiumImporterFactory : BlinkImporterFactory
+    {
+#pragma warning disable 1591
+        public EdgeChromiumImporterFactory() : base("MicrosoftEdge", "%LOCALAPPDATA%\\Microsoft\\Edge\\User Data", 1) { }
+#pragma warning restore 1591
+    }
+}
